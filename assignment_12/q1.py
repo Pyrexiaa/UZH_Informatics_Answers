0ad00141fd5d036bde5b831d158c9a68d73bfcdf
jiayu.lim@uzh.ch <U2102820@siswa.um.edu.my>
2023-12-13 17:33:54 +0100

    update q1.py

--- assignment_12/q1.py
+++ assignment_12/q1.py
@@ -26,6 +26,10 @@ def hamming_dist(signal_1, signal_2):
     
     output = []
     for i in range(len(data1)):
+
+        if len(data1[i]) == 0 or len(data2[i]) == 0:
+            return "Empty signal on at least one of the sensors"
+        
         if len(data1[i]) != len(data2[i]):
             return "Sensor defect detected"
         
@@ -41,5 +45,5 @@ def hamming_dist(signal_1, signal_2):
 
 signal_sensor_1 = {"times": [0, 1, 2, 3, 4, 5],
                    "data": ["00101110", "11001011", "11110000", "01000011", "11001101", "00011011"]}
-signal_sensor_2 = ("00101110", "11001001", "11110011", "01111011", "11001101", "00011011")
+signal_sensor_2 = ("", "11001001", "11110011", "01111011", "11001101", "00011011")
 print(hamming_dist(signal_sensor_1, signal_sensor_2))
